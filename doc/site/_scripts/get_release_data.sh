d3b6bbbad1f21bd5c8884a74dcbcb132985bd123
Slashscreen <SlashScreen@users.noreply.github.com>
2025-05-05 19:27:01 -0700

    Update get_release_data.sh

--- doc/site/_scripts/get_release_data.sh
+++ doc/site/_scripts/get_release_data.sh
@@ -6,9 +6,5 @@ DATA_FILE="$SCRIPT_DIR/../_data/latest_release.json"
 echo "> writing $DATA_FILE"
 
 rm $DATA_FILE
-<<<<<<< HEAD
-curl -s https://api.github.com/repos/beyond-all-reason/RecoilEngine/releases/latest \
-=======
 curl -s https://api.github.com/repos/beyond-all-reason/RecoilEngine/releases/latest  \
->>>>>>> 0aa5469497c42b9066a304f13f76ea460aa69b07
   > $DATA_FILE
